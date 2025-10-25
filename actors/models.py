494b0be37d9179473d635051ae77c0a3c8920e58
Marco Silva <marcosilvam159@gmail.com>
2025-10-25 20:33:58 -0300

    actors's app created, models.py config and migrate

---
+++ actors/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+NATIONALITY_CHOICES = (
+    ('USA','Estados Unidos'),
+    ('BR','Brasil'),
+)
+
+class Actors(models.Model):
+    name = models.CharField(max_length=200)
+    birthdate = models.DateField(blank=True, null=True)
+    nationality = models.CharField(
+        max_length=100,
+        choices=NATIONALITY_CHOICES,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+    
