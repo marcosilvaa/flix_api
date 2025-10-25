d68ca00aec70685104fe0e7462152c34590b1614
Marco Silva <marcosilvam159@gmail.com>
2025-10-25 18:03:31 -0300

    creted GET & POST method

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
-
-def hello_view(request):
-    return JsonResponse({'message':'Hellow World'})
+from genres.views import genre_create_list_view, genre_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_view)
+    path('genres/', genre_create_list_view, name='genre-create-list'),
+    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+
 ]
