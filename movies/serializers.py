9fbe7e9f1353d2156535478d751d5d5225dad0c4
Marco Silva <marcosilvam159@gmail.com>
2025-10-27 09:38:51 -0300

    re-building serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from movies.models import Movie
 
-class MovieSerializer(serializers.ModelSerializer):
+    
+class MovieModelSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Movie
         fields = '__all__'
+        
+    rate = serializers.SerializerMethodField(read_only=True)
+    def get_rate(self, obj):
+        return obj.release_date
+        
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior à 1990.')
+        return value 
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('O resumo nao pode ser maior do que 200 carcteres.')
+        return value
+        
