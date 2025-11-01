6b1dc33577c3c75fa131b6ab6f84638e33fafbc5
Marco Silva <marcosilvam159@gmail.com>
2025-11-01 11:19:22 -0300

    authentication fully config, with get token, refresh and verify token

--- actors/views.py
+++ actors/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 from actors.models import Actor
 from actors.serializers import ActorSerializer
 
