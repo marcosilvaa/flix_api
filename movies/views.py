a3267b99adf8d4c4e6bcbdfee363882e10f39a77
Marco Silva <marcosilvam159@gmail.com>
2025-12-12 22:34:47 -0300

    update

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
@@ -15,3 +16,19 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated,GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
+    
+    
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+    
+    
+    def get(self, request):
+        
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        
+        return response.Response(
+            data = {'message', 'funcionou'},
+            status = status.HTTP_200_OK
+        )
