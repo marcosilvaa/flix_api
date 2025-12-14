1adf957d7ff02a15b02ff2774ba4d0084144b4fb
Marco Silva <marcosilvam159@gmail.com>
2025-12-13 23:31:56 -0300

    DEBUG False

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tb4x+=@@#^j@wb81#7^h#2ps7hx&oq2t&@ha+#pz7v7e5!@hli'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
