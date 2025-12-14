fb736085901396f36b7611161a2a55f6db883a79
Marco Silva <marcosilvam159@gmail.com>
2025-12-13 21:14:37 -0300

    update

--- movies/views.py
+++ movies/views.py
@@ -2,9 +2,9 @@ from django.db.models import Count
 from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
-from movies.serializers import MovieModelSerializer
+from movies.serializers import MovieModelSerializer, MovieListDetailSerializer, MovieStatsSerializer
 from app.permissions import GlobalDefaultPermission
-
+from reviews.models import Review
 
 class MovieCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,GlobalDefaultPermission,)
@@ -24,11 +24,21 @@ class MovieStatsView(views.APIView):
     
     
     def get(self, request):
-        
         total_movies = self.queryset.count()
         movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
-        
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+        data = {
+            'total_movies': total_movies,
+            'movies_by_genre': movies_by_genre,
+            'total_reviews': total_reviews,
+            'average_stars': round(average_stars, 1) if average_stars else 0,
+        }
+        serializer = MovieStatsSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
         return response.Response(
-            data = {'message', 'funcionou'},
-            status = status.HTTP_200_OK
+            data=serializer.validated_data,
+            status=status.HTTP_200_OK,
         )
